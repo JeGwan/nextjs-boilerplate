0e2cb613cdac90b8ef361838c261c7cc2ea49640
JeGwan <emperorvv@gmail.com>
2021-03-11 02:13:42 +0900

    .env.js 로 환경변수 js 객체로 선언, next.config.js 에서 해당객체를 임포트해서 환경변수로 넣기

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 const withLess = require("@zeit/next-less");
 const withCSS = require("@zeit/next-css");
-
+const env = require("./.env.js");
 module.exports = withCSS(
-  withLess({
-    env: {
-      // 환경변수 SSR CSR 동시 지급하기 위해선 여기에
-      END_POINT: process.env.END_POINT,
-    },
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-    },
-  })
+  withLess({ env, lessLoaderOptions: { javascriptEnabled: true } })
 );
