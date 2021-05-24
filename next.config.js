055f7ca0cc48b1828e7334454771a78f6e54dc2a
O Je-Gwan <emperorvv@gmail.com>
2021-05-24 17:14:02 +0900

    feat: webpack-bundle-analyzer 도입

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
-module.exports = {
+const withBundleAnalyzer = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (process.env.ANALYZE === "true") {
+        const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+        config.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            reportFilename: options.isServer
+              ? "../analyze/server.html"
+              : "./analyze/client.html",
+          })
+        );
+      }
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
+      return config;
+    },
+  });
+};
+
+const nextConfig = {
   env: require("./.env.js"),
 };
+
+module.exports = withBundleAnalyzer(nextConfig);
